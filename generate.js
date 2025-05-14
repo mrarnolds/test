80b86ee677ba2a02d8c555c6bbbd9a6d389a3ca4
Arnolds Verins <arnolds.verins@gmail.com>
2025-05-14 17:07:14 +0200

    ci: fix flow

--- generate.js
+++ generate.js
@@ -2,6 +2,6 @@
 const fs = require("fs");
 
 const randomData = Math.random().toString(36).substring(2, 15);
-fs.writeFileSync("test.txt", `Random data: ${randomData}\n`, "utf8");
+fs.writeFileSync("CHANGELOG_PUBLIC.md", `Random data: ${randomData}\n`, "utf8");
 
 console.log("✅ test.txt generated with random data.");
